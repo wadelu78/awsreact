194fbec7430c00dd82e6f6280b4970dadee16ad9
Wei Lu <wadelu0714@gmail.com>
2021-03-31 00:41:29 +1300

    Initial the main UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,75 @@
-import logo from './logo.svg'
 import './App.css'
+import {
+  Typography,
+  AppBar,
+  Button,
+  IconButton,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from '@material-ui/core'
+import { Dashboard, Memory, Add } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles'
+import GridLayout from 'react-grid-layout'
+import '../node_modules/react-grid-layout/css/styles.css'
+import '../node_modules/react-resizable/css/styles.css'
 
 function App() {
-  return <h1>This is my react application</h1>
+  const classes = useStyles()
+  const layout = [
+    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+    { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+  ]
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton color='inherit'>
+            <Dashboard />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            Services Dashboard
+          </Typography>
+
+          <IconButton color='inherit'>
+            <Add />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <GridLayout
+          className='layout'
+          layout={layout}
+          cols={12}
+          rowHeight={30}
+          width={1200}
+        >
+          <div key='a'>a</div>
+          <div key='b'>b</div>
+          <div key='c'>c</div>
+        </GridLayout>
+      </main>
+    </>
+  )
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
 export default App
